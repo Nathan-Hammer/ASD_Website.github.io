d711b74b7c5421f87415606d2bb0c4754e9a7528
Nathan-Hammer <83573151+Nathan-Hammer@users.noreply.github.com>
2024-05-19 19:53:09 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+var formdata = new FormData();
+formdata.append("type", "Thick Horizontal, Horizontal Strip");
+formdata.append("tags", "car,maintanence,blog,automotive,tipd");
+
+var ajax = new XMLHttpRequest();
+ajax.addEventListener("load", completeHandler, false);
+
+ajax.open("POST", "https://ad.simaneka.com/api/get");
+ajax.setRequestHeader("authorisation", "oK8T7ocigzWnY0RGNv5UzfBx33GSEFa5");
+
+ajax.send(formdata);
+
+function completeHandler(event) {
+    var response = JSON.parse(event.target.responseText);
+
+    console.log(response);
+    document.querySelector('.advertIMG').src = response.link;
+    document.querySelector('.advertIMG').alt = response.alt;
+    document.querySelector('.anchorElement').href = response.href;
+    document.querySelector('.headerText').innerHTML = response.message;
+}
